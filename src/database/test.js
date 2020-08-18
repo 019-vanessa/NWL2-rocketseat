606952aa9420b84afd87656b4cd8bc344a2b4bb7
Vanessa Augutis <vanessaaugutis@gmail.com>
2020-08-18 12:44:51 -0300

    Finalização do projeto

--- src/database/test.js
+++ src/database/test.js
@@ -11,8 +11,8 @@ Database.then(async (db) => {
     
   }
   classValue = {
-    subject:"Química",
-    cost:"40",
+    subject: 1,
+    cost:"20",
     //proffy id virá do banco de dados
     
   }
@@ -31,5 +31,27 @@ Database.then(async (db) => {
     }
   ]
   
-  await createProffy(db, {proffyValue,classValue,classScheduleValues})
+  //await createProffy(db, {proffyValue,classValue,classScheduleValues})
+  
+  const selectedProffys = await db.all("SELECT * FROM proffys")
+  //console.log(selectedProffys)
+  
+  const selectedClassesAndProffys = await db.all(`
+    SELECT classes.*,proffys.*
+    FROM proffys
+    JOIN classes ON (classes.proffy_id = proffys.id)
+    WHERE classes.proffy_id = 1;
+  `)
+  //console.log(selectedClassesAndProffys)
+  
+  const selectClassesSchedules = await db.all(`
+    SELECT class_schedule.*
+    FROM class_schedule
+    WHERE class_schedule.class_id = "1"
+    AND class_schedule.weekday = "0"
+    AND class_schedule.time_from <= "1300"
+    AND class_schedule.time_to > "1300"
+  `)
+  
+  //console.log(selectClassesSchedules)
 })
